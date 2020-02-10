e88477b29cf0b179339f99487c78d28be7d3d94f
tuntuntuntuntun <tuntuntun02b@gmail.com>
2020-02-10 21:16:08 +0900

    共通部分を別のファイルへ

--- resources/views/bill/index.blade.php
+++ resources/views/bill/index.blade.php
@@ -5,17 +5,7 @@
         <div class="container">
             <p>受け取る金額: {{ $receive }}円</p>
             @foreach($bills as $bill)
-                <div class="mt-3 card">
-                    <div class="card-body">
-                        <p class="card-text">{{ $bill->title }}</p>
-                        <p class="card-text">合計: {{ $bill->total }}円</p>
-                        <p class="card-text">割り勘をした相手: 
-                            @foreach($to_user_names as $to_user_name)
-                                {{ $to_user_name }}
-                            @endforeach
-                        </p>
-                    </div>
-                </div>
+                @include('share.bill')
             @endforeach
         </div>
     </main>
