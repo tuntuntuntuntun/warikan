c3741d84ead89489bbb4bc0747f23828b96f402c
tuntuntuntuntun <tuntuntun02b@gmail.com>
2020-02-11 23:51:53 +0900

    @includeを削除

--- resources/views/bill/index.blade.php
+++ resources/views/bill/index.blade.php
@@ -5,7 +5,20 @@
         <div class="container">
             <p>受け取る金額: {{ $receive }}円</p>
             @foreach($bills as $bill)
-                @include('share.bill')
+                @if($bill->user_id === Auth::id())
+                    <div class="mt-3 card">
+                        <div class="card-body">
+                            <p class="car-text">お金を払った人: {{ $bill->user->name }}</p>
+                            <p class="card-text">{{ $bill->title }}</p>
+                            <p class="card-text">合計: {{ $bill->total }}円</p>
+                            <p class="card-text">割り勘をした相手: 
+                                @foreach($to_user_names as $to_user_name)
+                                    {{ $to_user_name }}
+                                @endforeach
+                            </p>
+                        </div>
+                    </div>
+                @endif
             @endforeach
         </div>
     </main>
